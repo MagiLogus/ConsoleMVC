39871d2c0fb880da9c20e030d7103e6c058575ad
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-05-25 10:07:19 -0300

    funcao corrigida

--- Controller/ProdutoController.cs
+++ Controller/ProdutoController.cs
@@ -5,27 +5,20 @@ namespace MVC_Console.Controller
 {
     public class ProdutoController
     {
-        //instância de objeto produto e produtoView
         Produto produto = new Produto();
         ProdutoView produtoView = new ProdutoView();
 
-        //método controlador para acessar a listagem de produtos
         public void Listar()
         {
-            //lista de produtos que recebe os itens obtidos pelo método Ler da model
             List<Produto> produtos = produto.Ler();
 
-            //chamada para o método Listar da View, passando como parâmetro a lista obtida acima
             produtoView.Listar(produtos);
         }
 
-        //método controlador para acessar a funcionalidade de cadastro de produtos
         public void Cadastrar()
         {
-            //objeto que vai armazenar o produto criado
             Produto novoProduto = produtoView.Cadastrar();
 
-            //chamada para o método de inserir da View, passando como parâmetro o objeto cadastrado
             produto.Inserir(novoProduto);
         }
     }
